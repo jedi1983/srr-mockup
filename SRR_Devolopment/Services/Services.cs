5dccfbb28992dac30458508eb0e60f889c6aeb45
jedi1983 <ragunan@hotmail.com>
2016-07-18 00:31:19 +0700

    firstCommit

---
+++ SRR_Devolopment/Services/Services.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SRR_Devolopment.Model;
+using System.Collections.ObjectModel;
+
+namespace SRR_Devolopment.Services
+{
+    /// <summary>
+    /// Interfaces For Accessing Data Model 
+    /// </summary>
+    /// Roland 2016
+    public interface IDataServices
+    {
+        ObservableCollection<SRR_M_User_Login_H> GetLoginID(String UserName,String Password);
+        bool saveLogin(string UserName, String Name, String Password);
+
+    }
+
+    class DataAccessServices : IDataServices
+
+    {
+       
+       //ctor
+        srr_devEntities DataContextService;
+        public DataAccessServices()
+        {
+           DataContextService = new srr_devEntities();
+        }
+
+        ObservableCollection<SRR_M_User_Login_H> GetLoginID(String UserName,String Password)
+        {
+            ObservableCollection<SRR_M_User_Login_H> dataLinq;
+
+            object linQtoDB = from tbl in DataContextService.SRR_M_User_Login_H where tbl.Name == UserName && tbl.Password == Password select tbl;
+
+
+
+            dataLinq = new ObservableCollection<SRR_M_User_Login_H>(linQtoDB);         
+
+        }
+    }
+}
