6eeb127e1703d1946e827d155db2506073ab52a9
jedi1983 <ragunan@hotmail.com>
2017-03-24 17:17:47 +0700

    adding screen and view model templating

---
+++ SRR_Devolopment/ViewModel/ApprovalViewModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Windows;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GalaSoft.MvvmLight;
+using SRR_Devolopment.Model;
+using SRR_Devolopment.Services;
+using GalaSoft.MvvmLight.Command;
+using System.Collections.ObjectModel;
+using System.Windows.Controls;
+using SRR_Devolopment.Views;
+using SRR_Devolopment.BaseLib.Class;
+using GalaSoft.MvvmLight.Messaging;
+using SRR_Devolopment.MessageInfrastructure;
+using System.Drawing;
+
+
+namespace SRR_Devolopment.ViewModel
+{
+    class ApprovalViewModel : ViewModelBased
+    {
+
+        #region "Properties"
+
+        #endregion
+
+        #region "Methods"
+
+        #endregion
+
+        #region "CTOR"
+
+        #endregion
+
+        #region "Grid Event"
+
+        #endregion
+    }
+}
