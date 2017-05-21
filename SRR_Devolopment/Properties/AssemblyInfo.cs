c7a1422e22d948f80e2339ae71390ec9ac5af325
jedi1983 <ragunan@hotmail.com>
2017-05-21 13:24:07 +0700

    InstallerClickOnceDeployed

--- SRR_Devolopment/Properties/AssemblyInfo.cs
+++ SRR_Devolopment/Properties/AssemblyInfo.cs
@@ -7,12 +7,12 @@ using System.Windows;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("SRR_Devolopment")]
+[assembly: AssemblyTitle("Koperasi")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("SRR_Devolopment")]
-[assembly: AssemblyCompany("Microsoft")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2016")]
+[assembly: AssemblyProduct("Koperasi")]
+[assembly: AssemblyCompany("SRR Development Team")]
+[assembly: AssemblyCopyright("SRR Development Team © 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
